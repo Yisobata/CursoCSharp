3d0c6b85fd65a84cf4653b1e68b0e3e2ae4e1dbd
Luiz Felipe Isobata Yoshiassu <luiz.isobata@gmail.com>
2020-01-09 22:00:25 -0300

    Criacao do Projeto da Aula 115 do curso de C Sharp

---
+++ Aula115/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Aula115.Entities;
+using Aula115.Entities.Enums;
+namespace Aula115
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Order order = new Order
+            {
+                Id = 1080,
+                Moment = DateTime.Now,
+                Status = OrderStatus.PendingPayment
+            };
+
+            Console.WriteLine(order);
+
+            string txt = OrderStatus.PendingPayment.ToString();
+            OrderStatus os = Enum.Parse<OrderStatus>("Delivered");
+
+            Console.WriteLine(txt);
+            Console.WriteLine(os);
+        }
+    }
+}
